20f0ccae583c0747bf73a2a75aa53b4fb5a4b0d6
Muhammad-1Haseeb <your-email@example.com>
2026-07-06 19:28:51 +0500

    Add conditionals problem 03

---
+++ conditionals/04_solution.py
@@ -0,0 +1,10 @@
+fruit = "Banana"
+color = "Green"
+
+if fruit == "Banana":
+    if color == "Green":
+        print("Unripe")
+    elif color == "yellow":
+        print("Ripe")
+    elif color == "brown":
+        print("OverRipe")
