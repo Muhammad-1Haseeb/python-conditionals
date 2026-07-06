2c500c5fb245363f73ed46f8058e38f8c6c47a08
Muhammad-1Haseeb <your-email@example.com>
2026-07-06 14:54:27 +0500

    Add conditionals problem 03

---
+++ conditionals/03_solution.py
@@ -0,0 +1,40 @@
+score = 3
+
+if score >=90 and score <100:
+    grade = "A"
+
+elif score >= 80 and score <90:
+    grade = "B"
+
+elif score >= 70 and score <80:
+    grade = "B"
+
+elif score >= 60 and score <70:
+    grade = "B"
+
+else:
+    grade = "F"
+
+print("Grade: ", grade)
+
+# def print_heart():
+#     heart = [
+#         "   💖💖     💖💖   ",
+#         " 💖    💖 💖    💖 ",
+#         "💖      💖      💖",
+#         "💖   U S W A    💖",
+#         "💖              💖",
+#         " 💖            💖 ",
+#         "  💖          💖  ",
+#         "   💖        💖   ",
+#         "    💖      💖    ",
+#         "     💖    💖     ",
+#         "      💖  💖      ",
+#         "       💖💖       "
+#     ]
+    
+#     for line in heart:
+#         print(line.center(40))
+
+# print("🌹 A Beautiful Heart for USWA 🌹\n")
+# print_heart()
